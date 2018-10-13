4165a0a1d1ebfcda7d41f3df7c7efd63e2c6df42
pognet <pogo@interbit.pl>
2018-10-13 12:03:07 +0200

    zmiana z liczb stałych na dowolne

--- kalkulator.c
+++ kalkulator.c
@@ -1,11 +1,30 @@
-#include "stdio.h"
+#include <stdio.h>
 
-int main() {
-    const int a = 4;
-    const int b = 7;
+int main ()
+{
+    int a, b, x;
+    char c;
 
-    printf("%i", a + b);
-    printf("%i", a - b);
+    printf("Wpisz pierwszą liczbę : \n");   // podaj pierwsza liczbe przypisana do a
+    scanf("%d", &a);
+    printf("Wpisz drugą liczbę: \n");       // podaj pierwsza liczbe przypisana do b
+    scanf("%d", &b);
+    getchar();
+    printf("Jaką operację chcesz wykonać (podaj znak): \n");
+    scanf("%c", &c);
 
+    switch (c)   /// działanie
+    {
+        case   '+': x=a+b;                  // operacja dodawania
+            printf("Wynik to: %d\n", x); break;
+        case   '-': x=a-b;                  // operacja odejmowania
+            printf("Wynik to: %d\n", x); break;
+        case   '*': x=a*b;                  // operacja mnożenia
+            printf("Wynik to: %d\n", x); break;
+        case   '/': x=a/b;                  // operacja dzielenia
+            printf("Wynik to: %d\n", x);
+
+    }
+    
     return 0;
 }
